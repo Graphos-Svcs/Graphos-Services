9f70959f941260a4769270d5c2d0621776b6338a
Graphos-Svcs <graphos.svcs@gmail.com>
2025-11-23 19:18:43 +0300

    Delete jsmain.js

--- jsmain.js
+++
@@ -1,8 +0,0 @@
-function toggleLanguage() {
-    alert("نظام اللغة العربية / الإنجليزية متوفر في النسخة القادمة PRO FULL");
-}
-
-function toggleAI() {
-    const box = document.getElementById("ai-box");
-    box.style.display = box.style.display === "block" ? "none" : "block";
-}
